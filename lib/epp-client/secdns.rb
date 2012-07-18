49d7caf7472e66edec3e2771a57687a89c16962b
Mathieu Arnold <mat@mat.cc>
2012-07-18 14:58:11 +0200

    Add domain:info secDNS values.

--- lib/epp-client/secdns.rb
+++ lib/epp-client/secdns.rb
@@ -13,4 +13,90 @@ module EPPClient::SecDNS
     super
     @extensions << EPPClient::SCHEMAS_URL['secDNS-1.1']
   end
+
+  # Extends the base domain info so that the specific secDNS elements
+  # can be added.
+  #
+  # either:
+  # [<tt>:keyData</tt>]
+  #   containing an array of keyData objects with the following fields :
+  #   [<tt>:flags</tt>]
+  #     The flags field value as described in {section 2.1.1 of RFC
+  #     4034}[http://tools.ietf.org/html/rfc4034#section-2.1.1].
+  #   [<tt>:protocol</tt>]
+  #     The protocol field value as described in {section 2.1.2 of RFC
+  #     4034}[http://tools.ietf.org/html/rfc4034#section-2.1.2].
+  #   [<tt>:alg</tt>]
+  #     The algorithm number field value as described in {section 2.1.3 of RFC
+  #     4034}[http://tools.ietf.org/html/rfc4034#section-2.1.3].
+  #   [<tt>:pubKey</tt>]
+  #     The encoded public key field value as described in {Section 2.1.4 of
+  #     RFC 4034}[http://tools.ietf.org/html/rfc4034#section-2.1.4].
+  # [<tt>:dsData</tt>]
+  #   containing an array of dsData objects with the following fields :
+  #   [<tt>:keyTag</tt>]
+  #     The key tag value as described in {Section 5.1.1 of RFC
+  #     4034}[http://tools.ietf.org/html/rfc4034#section-5.1.1].
+  #   [<tt>:alg</tt>]
+  #     The algorithm value as described in {Section 5.1.2 of RFC
+  #     4034}[http://tools.ietf.org/html/rfc4034#section-5.1.2].
+  #   [<tt>:digestType</tt>]
+  #     The digest type value as described in {Section 5.1.3 of RFC
+  #     4034}[http://tools.ietf.org/html/rfc4034#section-5.1.3].
+  #   [<tt>:digest</tt>]
+  #     The digest value as described in {Section 5.1.1 of RFC
+  #     4034}[http://tools.ietf.org/html/rfc4034#section-5.1.1].
+  #   [<tt>:keyData</tt>]
+  #     An optional element that describes the key data used as input in the DS
+  #     hash calculation for use in server validation. The <tt>:keyData</tt>
+  #     element contains the child elements defined above.
+  #
+  # Optionnaly :
+  # [<tt>:maxSigLife</tt>]
+  #   An element that indicates a child's preference for the number of seconds
+  #   after signature generation when the parent's signature on the DS
+  #   information provided by the child will expire.
+  def domain_info(domain)
+    super # placeholder so that I can add some doc
+  end
+
+  def domain_info_process(xml) #:nodoc:
+    ret = super
+    ret_secdns = {}
+    if (maxSigLife = xml.xpath('epp:extension/secDNS:infData/secDNS:maxSigLife', EPPClient::SCHEMAS_URL)).size > 0
+      ret_secdns[:maxSigLife] = maxSigLife.text
+    end
+    ret_secdns[:dsData] = xml.xpath('epp:extension/secDNS:infData/secDNS:dsData', EPPClient::SCHEMAS_URL).map do |s|
+      parse_ds_data(s)
+    end
+    ret_secdns[:keyData] = xml.xpath('epp:extension/secDNS:infData/secDNS:keyData', EPPClient::SCHEMAS_URL).map do |s|
+      parse_key_data(s)
+    end
+
+    ret[:secDNS] = ret_secdns unless ret_secdns.values.reject {|v| v.nil?}.size == 0
+    ret
+  end
+
+  private
+  def parse_key_data(xml)
+    {
+      :flags => xml.xpath("secDNS:flags", EPPClient::SCHEMAS_URL).text.to_i,
+      :protocol => xml.xpath("secDNS:protocol", EPPClient::SCHEMAS_URL).text.to_i,
+      :alg => xml.xpath("secDNS:alg", EPPClient::SCHEMAS_URL).text.to_i,
+      :pubKey => xml.xpath("secDNS:pubKey", EPPClient::SCHEMAS_URL).text,
+    }
+  end
+  def parse_ds_data(xml)
+    ret = {
+      :keyTag => xml.xpath("secDNS:keyTag", EPPClient::SCHEMAS_URL).text.to_i,
+      :alg => xml.xpath("secDNS:alg", EPPClient::SCHEMAS_URL).text.to_i,
+      :digestType => xml.xpath("secDNS:digestType", EPPClient::SCHEMAS_URL).text.to_i,
+      :digest => xml.xpath("secDNS:digest", EPPClient::SCHEMAS_URL).text
+    }
+    if (keyData = xml.xpath('secDNS:keyData', EPPClient::SCHEMAS_URL)).size > 0
+      ret[:keyData] = parse_key_data(keyData)
+    end
+    ret
+  end
+
 end
