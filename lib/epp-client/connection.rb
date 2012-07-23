5bc8079f501df3f1fd83159fae7976a1914ae1b3
Mathieu Arnold <mat@mat.cc>
2012-07-23 10:47:28 +0200

    Split all the EPPClient::Foo in module EPPClient / Foo.

--- lib/epp-client/connection.rb
+++ lib/epp-client/connection.rb
@@ -1,76 +1,78 @@
-module EPPClient::Connection
+module EPPClient
+  module Connection
 
-  attr_reader :sent_frame, :recv_frame, :srv_version, :srv_lang, :srv_ns, :srv_ext
+    attr_reader :sent_frame, :recv_frame, :srv_version, :srv_lang, :srv_ns, :srv_ext
 
-  # Establishes the connection to the server, if successful, will return the
-  # greeting frame.
-  def open_connection
-    @tcpserver = TCPSocket.new(server, port)
-    @socket = OpenSSL::SSL::SSLSocket.new(@tcpserver, @context)
+    # Establishes the connection to the server, if successful, will return the
+    # greeting frame.
+    def open_connection
+      @tcpserver = TCPSocket.new(server, port)
+      @socket = OpenSSL::SSL::SSLSocket.new(@tcpserver, @context)
 
-    # Synchronously close the connection & socket
-    @socket.sync_close
+      # Synchronously close the connection & socket
+      @socket.sync_close
 
-    # Connect
-    @socket.connect
+      # Connect
+      @socket.connect
 
-    # Get the initial greeting frame
-    greeting_process(get_frame)
-  end
-
-  def greeting_process(xml)
-    @srv_version = xml.xpath('epp:epp/epp:greeting/epp:svcMenu/epp:version', EPPClient::SCHEMAS_URL).map {|n| n.text}
-    @srv_lang = xml.xpath('epp:epp/epp:greeting/epp:svcMenu/epp:lang', EPPClient::SCHEMAS_URL).map {|n| n.text}
-    @srv_ns = xml.xpath('epp:epp/epp:greeting/epp:svcMenu/epp:objURI', EPPClient::SCHEMAS_URL).map {|n| n.text}
-    if (ext = xml.xpath('epp:epp/epp:greeting/epp:svcMenu/epp:svcExtension/epp:extURI', EPPClient::SCHEMAS_URL)).size > 0
-      @srv_ext = ext.map {|n| n.text}
+      # Get the initial greeting frame
+      greeting_process(get_frame)
     end
 
-    return xml
-  end
-
-  # Gracefully close the connection
-  def close_connection
-    if defined?(@socket) and @socket.is_a?(OpenSSL::SSL::SSLSocket)
-      @socket.close
-      @socket = nil
-    end
-
-    if defined?(@tcpserver) and @tcpserver.is_a?(TCPSocket)
-      @tcpserver.close
-      @tcpserver = nil
-    end
-
-    return true if @tcpserver.nil? and @socket.nil?
-  end
-
-  # Sends a frame and returns the server's answer
-  def send_request(xml)
-    send_frame(xml)
-    get_frame
-  end
-
-  # sends a frame
-  def send_frame(xml)
-    @sent_frame = xml
-    @socket.write([xml.size + 4].pack("N") + xml)
-    sent_frame_to_xml
-    return
-  end
-
-  # gets a frame from the socket and returns the parsed response.
-  def get_frame
-    size = @socket.read(4)
-    if size.nil?
-      if @socket.eof?
-        raise SocketError, "Connection closed by remote server"
-      else
-        raise SocketError, "Error reading frame from remote server"
+    def greeting_process(xml)
+      @srv_version = xml.xpath('epp:epp/epp:greeting/epp:svcMenu/epp:version', EPPClient::SCHEMAS_URL).map {|n| n.text}
+      @srv_lang = xml.xpath('epp:epp/epp:greeting/epp:svcMenu/epp:lang', EPPClient::SCHEMAS_URL).map {|n| n.text}
+      @srv_ns = xml.xpath('epp:epp/epp:greeting/epp:svcMenu/epp:objURI', EPPClient::SCHEMAS_URL).map {|n| n.text}
+      if (ext = xml.xpath('epp:epp/epp:greeting/epp:svcMenu/epp:svcExtension/epp:extURI', EPPClient::SCHEMAS_URL)).size > 0
+	@srv_ext = ext.map {|n| n.text}
       end
-    else
-      size = size.unpack('N')[0]
-      @recv_frame = @socket.read(size - 4)
-      recv_frame_to_xml
+
+      return xml
+    end
+
+    # Gracefully close the connection
+    def close_connection
+      if defined?(@socket) and @socket.is_a?(OpenSSL::SSL::SSLSocket)
+	@socket.close
+	@socket = nil
+      end
+
+      if defined?(@tcpserver) and @tcpserver.is_a?(TCPSocket)
+	@tcpserver.close
+	@tcpserver = nil
+      end
+
+      return true if @tcpserver.nil? and @socket.nil?
+    end
+
+    # Sends a frame and returns the server's answer
+    def send_request(xml)
+      send_frame(xml)
+      get_frame
+    end
+
+    # sends a frame
+    def send_frame(xml)
+      @sent_frame = xml
+      @socket.write([xml.size + 4].pack("N") + xml)
+      sent_frame_to_xml
+      return
+    end
+
+    # gets a frame from the socket and returns the parsed response.
+    def get_frame
+      size = @socket.read(4)
+      if size.nil?
+	if @socket.eof?
+	  raise SocketError, "Connection closed by remote server"
+	else
+	  raise SocketError, "Error reading frame from remote server"
+	end
+      else
+	size = size.unpack('N')[0]
+	@recv_frame = @socket.read(size - 4)
+	recv_frame_to_xml
+      end
     end
   end
 end
--- lib/epp-client/smallregistry.rb
+++ lib/epp-client/smallregistry.rb
@@ -1,172 +1,174 @@
 require 'epp-client/base'
 
-class EPPClient::SmallRegistry < EPPClient::Base
+module EPPClient
+  class SmallRegistry < Base
 
-  SCHEMAS_SR = %w[
-    sr-1.0
-  ]
+    SCHEMAS_SR = %w[
+      sr-1.0
+    ]
 
-  EPPClient::SCHEMAS_URL.merge!(SCHEMAS_SR.inject({}) do |a,s|
-    a[s.sub(/-1\.0$/, '')] = "https://www.smallregistry.net/schemas/#{s}.xsd" if s =~ /-1\.0$/
-    a[s] = "https://www.smallregistry.net/schemas/#{s}.xsd"
-    a
-  end)
+    EPPClient::SCHEMAS_URL.merge!(SCHEMAS_SR.inject({}) do |a,s|
+      a[s.sub(/-1\.0$/, '')] = "https://www.smallregistry.net/schemas/#{s}.xsd" if s =~ /-1\.0$/
+      a[s] = "https://www.smallregistry.net/schemas/#{s}.xsd"
+      a
+    end)
 
-  #
-  # Sets the default for Smallregistry, that is, server and port, according
-  # to Smallregistry's documentation.
-  # https://www.smallregistry.net/faqs/quelles-sont-les-specificites-du-serveur-epp
-  #
-  # ==== Required Attributes
-  #
-  # [<tt>:client_id</tt>] the tag or username used with <tt><login></tt> requests.
-  # [<tt>:password</tt>] the password used with <tt><login></tt> requests.
-  # [<tt>:ssl_cert</tt>] the file containing the client certificate.
-  # [<tt>:ssl_key</tt>] the file containing the key of the certificate.
-  #
-  # ==== Optional Attributes
-  # [<tt>:test</tt>] sets the server to be the test server.
-  #
-  # See EPPClient for other attributes.
-  def initialize(attrs)
-    unless attrs.key?(:client_id) && attrs.key?(:password) && attrs.key?(:ssl_cert) && attrs.key?(:ssl_key)
-      raise ArgumentError, "client_id, password, ssl_cert and ssl_key are required"
-    end
-    if attrs.delete(:test) == true
-      attrs[:server] ||= 'epp.test.smallregistry.net'
-      attrs[:port] ||= 2700
-    else
-      attrs[:server] ||= 'epp.smallregistry.net'
-      attrs[:port] ||= 700
-    end
-    @services = EPPClient::SCHEMAS_URL.values_at('domain', 'contact')
-    super(attrs)
-    @extensions << EPPClient::SCHEMAS_URL['sr']
-  end
-
-  # Extends the base contact info so that the specific smallregistry's
-  # informations are processed, the additionnal informations are :
-  #
-  # one of :
-  # [<tt>:org</tt>]
-  #   indicating that the contact is an organisation with the following
-  #   informations :
-  #   [<tt>:companySerial</tt>]
-  #     the company's SIREN / RPPS / whatever serial number is required.
-  # [<tt>:person</tt>]
-  #   indicating that the contact is a human person with the following
-  #   informations :
-  #   [<tt>:birthDate</tt>] the person's birth date.
-  #   [<tt>:birthPlace</tt>] the person's birth place.
-  def contact_info(xml)
-    super # placeholder so that I can add some doc
-  end 
-
-  def contact_info_process(xml) #:nodoc:
-    ret = super
-    if (contact = xml.xpath('epp:extension/sr:ext/sr:infData/sr:contact', EPPClient::SCHEMAS_URL)).size > 0
-      if (person = contact.xpath('sr:person', EPPClient::SCHEMAS_URL)).size > 0
-        ret[:person] = {
-          :birthDate => Date.parse(person.xpath('sr:birthDate', EPPClient::SCHEMAS_URL).text),
-          :birthPlace => person.xpath('sr:birthPlace', EPPClient::SCHEMAS_URL).text,
-        }
+    #
+    # Sets the default for Smallregistry, that is, server and port, according
+    # to Smallregistry's documentation.
+    # https://www.smallregistry.net/faqs/quelles-sont-les-specificites-du-serveur-epp
+    #
+    # ==== Required Attributes
+    #
+    # [<tt>:client_id</tt>] the tag or username used with <tt><login></tt> requests.
+    # [<tt>:password</tt>] the password used with <tt><login></tt> requests.
+    # [<tt>:ssl_cert</tt>] the file containing the client certificate.
+    # [<tt>:ssl_key</tt>] the file containing the key of the certificate.
+    #
+    # ==== Optional Attributes
+    # [<tt>:test</tt>] sets the server to be the test server.
+    #
+    # See EPPClient for other attributes.
+    def initialize(attrs)
+      unless attrs.key?(:client_id) && attrs.key?(:password) && attrs.key?(:ssl_cert) && attrs.key?(:ssl_key)
+	raise ArgumentError, "client_id, password, ssl_cert and ssl_key are required"
       end
-      if (org = contact.xpath('sr:org', EPPClient::SCHEMAS_URL)).size > 0
-        ret[:org] = { :companySerial => org.xpath('sr:companySerial', EPPClient::SCHEMAS_URL).text }
+      if attrs.delete(:test) == true
+	attrs[:server] ||= 'epp.test.smallregistry.net'
+	attrs[:port] ||= 2700
+      else
+	attrs[:server] ||= 'epp.smallregistry.net'
+	attrs[:port] ||= 700
       end
-    end
-    ret
-  end
-
-  # Extends the base contact create so that the specific smallregistry's
-  # information are sent, the additionnal informations are :
-  #
-  # one of :
-  # [<tt>:org</tt>]
-  #   indicating that the contact is an organisation with the following
-  #   informations :
-  #   [<tt>:companySerial</tt>]
-  #     the company's SIREN / RPPS / whatever serial number is required.
-  # [<tt>:person</tt>]
-  #   indicating that the contact is a human person with the following
-  #   informations :
-  #   [<tt>:birthDate</tt>] the person's birth date.
-  #   [<tt>:birthPlace</tt>] the person's birth place.
-  def contact_create(contact)
-    super # placeholder so that I can add some doc
-  end
-
-  def contact_create_xml(contact) #:nodoc:
-    ret = super
-
-    ext = extension do |xml|
-      xml.ext( :xmlns => EPPClient::SCHEMAS_URL['sr']) do
-        xml.create do
-          xml.contact do
-            if contact.key?(:org)
-              xml.org do
-                xml.companySerial(contact[:org][:companySerial])
-              end
-            elsif contact.key?(:person)
-              xml.person do
-                xml.birthDate(contact[:person][:birthDate])
-                xml.birthPlace(contact[:person][:birthPlace])
-              end
-            end
-          end
-        end
-      end
+      @services = EPPClient::SCHEMAS_URL.values_at('domain', 'contact')
+      super(attrs)
+      @extensions << EPPClient::SCHEMAS_URL['sr']
     end
 
-    insert_extension(ret, ext)
-  end
+    # Extends the base contact info so that the specific smallregistry's
+    # informations are processed, the additionnal informations are :
+    #
+    # one of :
+    # [<tt>:org</tt>]
+    #   indicating that the contact is an organisation with the following
+    #   informations :
+    #   [<tt>:companySerial</tt>]
+    #     the company's SIREN / RPPS / whatever serial number is required.
+    # [<tt>:person</tt>]
+    #   indicating that the contact is a human person with the following
+    #   informations :
+    #   [<tt>:birthDate</tt>] the person's birth date.
+    #   [<tt>:birthPlace</tt>] the person's birth place.
+    def contact_info(xml)
+      super # placeholder so that I can add some doc
+    end 
 
-  def contact_update_xml(args) #:nodoc:
-    ret = super
+    def contact_info_process(xml) #:nodoc:
+      ret = super
+      if (contact = xml.xpath('epp:extension/sr:ext/sr:infData/sr:contact', EPPClient::SCHEMAS_URL)).size > 0
+	if (person = contact.xpath('sr:person', EPPClient::SCHEMAS_URL)).size > 0
+	  ret[:person] = {
+	    :birthDate => Date.parse(person.xpath('sr:birthDate', EPPClient::SCHEMAS_URL).text),
+	    :birthPlace => person.xpath('sr:birthPlace', EPPClient::SCHEMAS_URL).text,
+	  }
+	end
+	if (org = contact.xpath('sr:org', EPPClient::SCHEMAS_URL)).size > 0
+	  ret[:org] = { :companySerial => org.xpath('sr:companySerial', EPPClient::SCHEMAS_URL).text }
+	end
+      end
+      ret
+    end
 
-    if args.key?(:chg) && (args[:chg].key?(:org) || args[:chg].key?(:person))
+    # Extends the base contact create so that the specific smallregistry's
+    # information are sent, the additionnal informations are :
+    #
+    # one of :
+    # [<tt>:org</tt>]
+    #   indicating that the contact is an organisation with the following
+    #   informations :
+    #   [<tt>:companySerial</tt>]
+    #     the company's SIREN / RPPS / whatever serial number is required.
+    # [<tt>:person</tt>]
+    #   indicating that the contact is a human person with the following
+    #   informations :
+    #   [<tt>:birthDate</tt>] the person's birth date.
+    #   [<tt>:birthPlace</tt>] the person's birth place.
+    def contact_create(contact)
+      super # placeholder so that I can add some doc
+    end
+
+    def contact_create_xml(contact) #:nodoc:
+      ret = super
+
       ext = extension do |xml|
-        xml.ext( :xmlns => EPPClient::SCHEMAS_URL['sr']) do
-          xml.update do
-            xml.contact do
-              if args[:chg].key?(:org)
-                xml.org do
-                  xml.companySerial(args[:chg][:org][:companySerial])
-                end
-              elsif args[:chg].key?(:person)
-                xml.person do
-                  xml.birthDate(args[:chg][:person][:birthDate])
-                  xml.birthPlace(args[:chg][:person][:birthPlace])
-                end
-              end
-            end
-          end
-        end
+	xml.ext( :xmlns => EPPClient::SCHEMAS_URL['sr']) do
+	  xml.create do
+	    xml.contact do
+	      if contact.key?(:org)
+		xml.org do
+		  xml.companySerial(contact[:org][:companySerial])
+		end
+	      elsif contact.key?(:person)
+		xml.person do
+		  xml.birthDate(contact[:person][:birthDate])
+		  xml.birthPlace(contact[:person][:birthPlace])
+		end
+	      end
+	    end
+	  end
+	end
       end
 
-      return insert_extension(ret, ext)
-    else
-      return ret
+      insert_extension(ret, ext)
     end
-  end
 
-  # Extends the base contact update so that the specific afnic update
-  # informations can be sent, the additionnal informations are :
-  #
-  # [<tt>:chg</tt>]
-  #   changes one of :
-  #   [<tt>:org</tt>]
-  #     indicating that the contact is an organisation with the following
-  #     informations :
-  #       [<tt>:companySerial</tt>]
-  #         the company's SIREN / RPPS / whatever serial number is required.
-  #   [<tt>:person</tt>]
-  #     indicating that the contact is a human person with the following
-  #     informations :
-  #     [<tt>:birthDate</tt>] the person's birth date.
-  #     [<tt>:birthPlace</tt>] the person's birth place.
-  #
-  def contact_update(args)
-    super # placeholder so that I can add some doc
+    def contact_update_xml(args) #:nodoc:
+      ret = super
+
+      if args.key?(:chg) && (args[:chg].key?(:org) || args[:chg].key?(:person))
+	ext = extension do |xml|
+	  xml.ext( :xmlns => EPPClient::SCHEMAS_URL['sr']) do
+	    xml.update do
+	      xml.contact do
+		if args[:chg].key?(:org)
+		  xml.org do
+		    xml.companySerial(args[:chg][:org][:companySerial])
+		  end
+		elsif args[:chg].key?(:person)
+		  xml.person do
+		    xml.birthDate(args[:chg][:person][:birthDate])
+		    xml.birthPlace(args[:chg][:person][:birthPlace])
+		  end
+		end
+	      end
+	    end
+	  end
+	end
+
+	return insert_extension(ret, ext)
+      else
+	return ret
+      end
+    end
+
+    # Extends the base contact update so that the specific afnic update
+    # informations can be sent, the additionnal informations are :
+    #
+    # [<tt>:chg</tt>]
+    #   changes one of :
+    #   [<tt>:org</tt>]
+    #     indicating that the contact is an organisation with the following
+    #     informations :
+    #       [<tt>:companySerial</tt>]
+    #         the company's SIREN / RPPS / whatever serial number is required.
+    #   [<tt>:person</tt>]
+    #     indicating that the contact is a human person with the following
+    #     informations :
+    #     [<tt>:birthDate</tt>] the person's birth date.
+    #     [<tt>:birthPlace</tt>] the person's birth place.
+    #
+    def contact_update(args)
+      super # placeholder so that I can add some doc
+    end
   end
 end
