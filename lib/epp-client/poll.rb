4ca115194d99ce1815ce5ca5c860d3bc0d1d5a2f
Mathieu Arnold <mat@mat.cc>
2016-02-12 17:18:26 +0100

    Style/MultilineOperationIndentation

--- lib/epp-client/poll.rb
+++ lib/epp-client/poll.rb
@@ -36,7 +36,7 @@ module EPPClient
         ret[:msg_xml] = msg.to_s
       end
       if (obj = xml.xpath('epp:resData', EPPClient::SCHEMAS_URL)).size > 0 ||
-        (obj = xml.xpath('epp:extension', EPPClient::SCHEMAS_URL)).size > 0
+         (obj = xml.xpath('epp:extension', EPPClient::SCHEMAS_URL)).size > 0
         ret[:obj_xml] = obj.to_s
         PARSERS.each do |xpath,parser|
           if obj.xpath(xpath, EPPClient::SCHEMAS_URL).size > 0
